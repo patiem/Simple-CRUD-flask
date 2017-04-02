cca0ec22e69dc9006e272a70e79f7be98b749c93
Pati Mikulska <pp.mikulska@gmail.com>
2017-04-02 12:52:40 +0200

    Delete comments

--- app.py
+++ app.py
@@ -1,22 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-# import os
-#
-# BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-#
-# app = Flask(__name__)
-#
-# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + BASE_DIR + "/db.db"
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#
-# db = SQLAlchemy(app)
-#
-# class Socks(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(30))
-#     description = db.Column(db.String(200))
-#     price = db.Column(db.Float)
-#     image = db.Column(db.String(100))
 
 app = Flask(__name__)
 app.config.from_object('config')
